99c3381e3ca9ced13cd6cd140ae3b9798ea6b59d
Rishat Murzyev <rmurzyev@mail.ru>
2022-05-23 12:42:05 +0500

    Changed methods to Read json files chenged method to open deposit chenged method to Write json file

--- Repositorys/DepositRepository.cs
+++ Repositorys/DepositRepository.cs
@@ -22,65 +22,61 @@ namespace Bank_A_WpfApp
 
         public List<Deposit> GetDeposits()
         {
-            var path = Path.Combine(Environment.CurrentDirectory, "DepositDB.json");
-            var sr = File.ReadAllText(path);
-            List<Deposit> deposit = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Deposit>>(sr);
-            return deposit;
+            List<Deposit> deposits = ReadJson(jsonFilePathDDB);
+            return deposits;
+        }
+
+        public void WriteJson(string filePath, List<Deposit> trackDataList)
+        {
+            string json = JsonConvert.SerializeObject(trackDataList);
+
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                sw.Write(json);
+            }
+        }
+
+        public List<Deposit> ReadJson(string filePath)
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string json = sr.ReadToEnd();
+
+                return JsonConvert.DeserializeObject<List<Deposit>>(json);
+            }
         }
 
         public List<Deposit> OpenDeposit()
         {
+            List<Deposit> myTrackDataList = ReadJson(jsonFilePathDDB);
 
             string[] DepositTypeArr = new string[2] { "Капитализированный", "Некапитализированный" };
-            string filePath = jsonFilePathDDB;
-                JsonSerializer jsonSerializer = JsonSerializer.Create(new JsonSerializerSettings
-                { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
-                using StreamWriter sw = new(filePath, true);
-                List<Deposit> deposit = new();
-                    deposit.Add(new Deposit()
-                    {
-                        DepositNumber = $"{rnd.Next(10)}{rnd.Next(10)}" +
-                                        $"{rnd.Next(10)}{rnd.Next(10)}" +
-                                        $"{rnd.Next(10)}{rnd.Next(10)}" +
-                                        $"{rnd.Next(10)}{rnd.Next(10)}" +
-                                        $"{rnd.Next(10)}{rnd.Next(10)}" +
-                                        $"{rnd.Next(10)}{rnd.Next(10)}" +
-                                        $"{rnd.Next(10)}{rnd.Next(10)}" +
-                                        $"{rnd.Next(10)}{rnd.Next(10)}",
-                        AmountFunds = rnd.Next(1000, 100000),
-                        DepositType = DepositTypeArr[rnd.Next(2)],
-                        ClientId = rnd.Next(1, 3)
-                    });
-                jsonSerializer.Serialize(sw, deposit);
-            return deposit;
-        }
 
-        //public List<Deposit> SaveChanges()
-        //{
-        //    JsonSerializer jsonSerializer = JsonSerializer.Create(new JsonSerializerSettings
-        //    { PreserveReferencesHandling = PreserveReferencesHandling.Objects });
-        //    using StreamWriter sw = new(jsonFilePathDDB, true);
-        //    List<Deposit> deposits = new List<Deposit>();
-        //    foreach (var deposit in deposits)
-        //    {
-        //        sw.WriteLine(new Deposit()
-        //        {
-        //            DepositNumber = deposit.DepositNumber,
-        //            AmountFunds = deposit.AmountFunds,
-        //            DepositType = deposit.DepositType,
-        //            ClientId = deposit.ClientId,
-        //        });
-        //    }
-        //    jsonSerializer.Serialize(sw, deposits);
-        //    return deposits;
-        //}
+            Deposit newTrackData = new Deposit();
+            newTrackData.DepositNumber = $"{rnd.Next(10)}{rnd.Next(10)}" +
+                                         $"{rnd.Next(10)}{rnd.Next(10)}" +
+                                         $"{rnd.Next(10)}{rnd.Next(10)}" +
+                                         $"{rnd.Next(10)}{rnd.Next(10)}" +
+                                         $"{rnd.Next(10)}{rnd.Next(10)}" +
+                                         $"{rnd.Next(10)}{rnd.Next(10)}" +
+                                         $"{rnd.Next(10)}{rnd.Next(10)}" +
+                                         $"{rnd.Next(10)}{rnd.Next(10)}";
+            newTrackData.AmountFunds = rnd.Next(1000, 100000);
+            newTrackData.DepositType = DepositTypeArr[rnd.Next(2)];
+            newTrackData.ClientId = rnd.Next(1, 3);
+
+            myTrackDataList.Add(newTrackData);
+
+            WriteJson(jsonFilePathDDB, myTrackDataList);
+            return myTrackDataList;
+        }
         #endregion
 
         #region конструкторы
         public DepositRepository()
         {
             Deposits = new List<Deposit>();
-            Deposits = GetDeposits();
+            Deposits = OpenDeposit();
         }
         #endregion
     }
